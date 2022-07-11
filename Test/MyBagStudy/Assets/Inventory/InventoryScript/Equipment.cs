571cdeef2b13f98c663a5f6eeb7ba9e110afa410
Fong <lbx1924@gmail.com>
2022-07-11 19:05:54 +0800

    update

--- Test/MyBagStudy/Assets/Inventory/InventoryScript/Equipment.cs
+++ Test/MyBagStudy/Assets/Inventory/InventoryScript/Equipment.cs
@@ -4,31 +4,25 @@ using UnityEngine;
 
 public class Equipment : MonoBehaviour
 {
-    public GameObject[] _temp;
-    public bool isfull;
+    public float num;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        double a = num;
+        print(a);
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0 ; i < _temp.Length ; i++)
-        {
-                    print(i);
 
-            if(_temp[i] == null)
-            {   
-                isfull = false;
-                break;
-            }
-            else if(_temp[i] != null)
-            {
-                isfull = true;
-            }
+    }
+    static int addition(int a)
+    {
+        int add = a+a;
 
-        }
+        
+        return add;
     }
 }
