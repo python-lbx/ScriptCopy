7592748d21fd9a8e73ba4884c67db2956f3e6b40
Fong <60109720+python-lbx@users.noreply.github.com>
2023-05-15 00:06:33 +0800

    update

--- Test/ForTest/ForTest/Assets/Script/Ball.cs
+++ Test/ForTest/ForTest/Assets/Script/Ball.cs
@@ -8,13 +8,15 @@ public class Ball : MonoBehaviour
     Collider2D coll;
     public SpriteRenderer SR;
 
-    public Color Tough_color;
-    public Color Not_Tough_color;
-    public Color Geted_color;
-    public Color NotGet_color;
+    //public Color Tough_color;
+    //public Color Not_Tough_color;
+    //public Color Seted_color;
+    //public Color NotSet_color;
     public bool here;
     public bool holded;
     public bool circle_here;
+    public bool touchable;
+    public string Key;
     //public float button_cd_time;
     //public float last_button_time;
     public GameObject player;
@@ -26,6 +28,7 @@ public class Ball : MonoBehaviour
     {
         coll = GetComponent<Collider2D>();
         SR = GetComponent<SpriteRenderer>();
+        touchable = true;
         //last_button_time = -button_cd_time;
     }
 
@@ -49,24 +52,23 @@ public class Ball : MonoBehaviour
             }
         }
 
-        if (holded)
+        if (holded && touchable)
         {
             transform.position = player.transform.GetChild(0).GetComponent<Transform>().position;
         }
-        else
+        
+        if(!holded && circle_here)
         {
-            transform.position = transform.position;
-        }
-
-        if(holded && circle_here && here)
-        {
-            if (Input.GetKeyDown(KeyCode.G))
+            if (Magic_Circle != null)
             {
-                transform.position = Magic_Circle.transform.GetChild(0).GetComponent<Transform>().position;
-                holded = !holded;
+                if (Key == Magic_Circle.GetComponent<Circle>().Lock)
+                {
+                    transform.position = Magic_Circle.transform.Find("Point").transform.position;
+                    this.gameObject.transform.SetParent(Magic_Circle.transform);
+                    touchable = false;
+                }
             }
         }
-
     }
 
     private void OnTriggerStay2D(Collider2D other) 
@@ -74,11 +76,11 @@ public class Ball : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             here = true;
-            SR.color = Tough_color;
+            //SR.color = Tough_color;
             player = other.gameObject;
         }
 
-        if(other.name == "Circle")
+        if(other.CompareTag("Magic_Circle"))
         {
             circle_here = true;
             Magic_Circle = other.gameObject;
@@ -90,11 +92,11 @@ public class Ball : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             here = false;
-            SR.color = Not_Tough_color;
+            //SR.color = Not_Tough_color;
             player = null;
         }
 
-        if (other.name == "Circle")
+        if (other.CompareTag("Magic_Circle"))
         {
             circle_here = false;
             Magic_Circle = null;
